543eda27a55e9a914df3db5c75be63ed19e11649
toyman640 <falakoadebayor@gmail.com>
2023-04-10 17:10:59 +0100

    correct linters errors

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-let ham = document.querySelector('#ham-btn');
-let content = document.querySelector('.first-section');
-let menuList = document.querySelector('.rogue')
-let restOfPage = document.querySelectorAll('.toggle-section')
-ham.addEventListener('click', function(event) {
-    content.classList.toggle('blend-mode')
-    menuList.classList.toggle('invisible')
-    for (let i=0; i<restOfPage.length; i++){
-        restOfPage[i].classList.toggle('invisible')
-    }
-    
-    
-})
-restOfPage = document.querySelectorAll('.toggle-section')
-// let closeButton = document.querySelector('#close_button');
-let listItems = document.querySelectorAll('.ham-li');
-for( let j = 0; j < listItems.length; j++){
-    listItems[j].addEventListener('click', function(event) {
-        content.classList.toggle('blend-mode')
-        menuList.classList.toggle('invisible')
-        for (let i=0; i<restOfPage.length; i++){
-            restOfPage[i].classList.toggle('invisible')
-    
-        }     
-    })
+const ham = document.querySelector('#ham-btn');
+const content = document.querySelector('.first-section');
+const menuList = document.querySelector('.rogue');
+let restOfPage = document.querySelectorAll('.toggle-section');
+
+function hamDisplay() {
+  content.classList.toggle('blend-mode');
+  menuList.classList.toggle('invisible');
+  for (let i = 0; i < restOfPage.length; i += 1) {
+    restOfPage[i].classList.toggle('invisible');
+  }
 }
+ham.addEventListener('click', hamDisplay);
 
-
-
-
-
+restOfPage = document.querySelectorAll('.toggle-section');
+const listItems = document.querySelectorAll('.ham-li');
+for (let j = 0; j < listItems.length; j += 1) {
+    function hamLinks() {
+        content.classList.toggle('blend-mode');
+        menuList.classList.toggle('invisible');
+        for (let i = 0; i < restOfPage.length; i += 1) {
+          restOfPage[i].classList.toggle('invisible');
+        }
+      }
+  listItems[j].addEventListener('click', hamLinks);
+}
