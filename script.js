d056cbe7edd4ed68ecb264901a7c65da3193844a
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-04-10 09:52:12 -0300

    Add close button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let ham = document.querySelector('#ham-btn');
 let content = document.querySelector('.first-section');
+let menuList = document.querySelector('.rogue')
 ham.addEventListener('click', function(event) {
     content.classList.toggle('blend-mode')
+    menuList.classList.toggle('invisible')
     console.log('click');
 })
 
