9c8da4a60bc6f4ce5426cd26fae681cbc6c590ef
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-04-10 10:30:21 -0300

    Add toggle invisible feature for all mobile-page

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let ham = document.querySelector('#ham-btn');
 let content = document.querySelector('.first-section');
 let menuList = document.querySelector('.rogue')
+let restOfPage = document.querySelectorAll('.toggle-section')
 ham.addEventListener('click', function(event) {
     content.classList.toggle('blend-mode')
     menuList.classList.toggle('invisible')
-    console.log('click');
+    for (let i=0; i<restOfPage.length; i++){
+        restOfPage[i].classList.toggle('invisible')
+    }
+    
+    
 })
+restOfPage = document.querySelectorAll('.toggle-section')
+let closeButton = document.querySelector('#close_button');
+closeButton.addEventListener('click', function(event) {
+    content.classList.toggle('blend-mode')
+    menuList.classList.toggle('invisible')
+    for (let i=0; i<restOfPage.length; i++){
+        restOfPage[i].classList.toggle('invisible')
 
+    }
+    
+})
