017af8b459969c998ddbe4ed5cc37f72f02e6599
toyman640 <falakoadebayor@gmail.com>
2023-04-10 15:06:38 +0100

    Add menu item click should close menu section

--- script.js
+++ script.js
@@ -12,13 +12,20 @@ ham.addEventListener('click', function(event) {
     
 })
 restOfPage = document.querySelectorAll('.toggle-section')
-let closeButton = document.querySelector('#close_button');
-closeButton.addEventListener('click', function(event) {
-    content.classList.toggle('blend-mode')
-    menuList.classList.toggle('invisible')
-    for (let i=0; i<restOfPage.length; i++){
-        restOfPage[i].classList.toggle('invisible')
-
-    }
+// let closeButton = document.querySelector('#close_button');
+let listItems = document.querySelectorAll('.ham-li');
+for( let j = 0; j < listItems.length; j++){
+    listItems[j].addEventListener('click', function(event) {
+        content.classList.toggle('blend-mode')
+        menuList.classList.toggle('invisible')
+        for (let i=0; i<restOfPage.length; i++){
+            restOfPage[i].classList.toggle('invisible')
     
-})
+        }     
+    })
+}
+
+
+
+
+
